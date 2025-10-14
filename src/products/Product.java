a95ad809dfae9774f543232d0d6540957b384ad2
snmoe <moesnosy2@gmail.com>
2025-10-14 23:49:38 +0300

    Product.java
    This class represents product information

--- src/products/Product.java
+++ src/products/Product.java
@@ -1,4 +1,34 @@
 package products;
 
 public class Product {
+    private String productID, productName, manufacturerName,supplierName;
+    private int quantity;
+    private float price;
+    
+    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price)
+    {
+        this.productID = productID;
+        this.manufacturerName = manufacturerName;
+        this.price = price;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.supplierName = supplierName;
+    }
+    
+    public int getQuantity()
+    {
+        return this.quantity;
+    }
+    public void setQuantity(int quantity)
+    {
+       this.quantity = quantity; 
+    }
+    public String lineRepresentation()
+    {
+        return productID + "," + productName + "," + manufacturerName + "," + supplierName + "," + quantity + "," + price;
+    }
+    public String getSearchKey()
+    {
+        return this.productID;
+    }
 }
