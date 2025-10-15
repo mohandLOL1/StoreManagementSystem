d6501cbef93fb7cc3d94b1edb53bee0cc5f4fb13
esm <moateez.ahmed@gmail.com>
2025-10-16 02:39:23 +0300

    Saving my local work before pulling

--- src/mai.java
+++ src/mai.java
@@ -1,16 +1,22 @@
 import databases.EmployeeUserDatabase;
+import roles.AdminRole;
 import users.EmployeeUser;
 
 import java.io.IOException;
 
 public class mai {
 
-    static void main(String[] args) throws IOException {
-        EmployeeUser e1 = new EmployeeUser("5","2","3","4","5");
-        EmployeeUserDatabase db = new EmployeeUserDatabase("Employees.txt");
+   public static void main(String[] args) throws IOException {
 
-        db.readFromFile();
-        db.insertRecord(e1);
-        db.saveToFile();
+       AdminRole a1=new AdminRole();
+
+       a1.addEmployee("  -1","lola","lola","jfff","efefef");
+
+         EmployeeUser[] array=a1.getListOfEmployees();
+       System.out.println(array[0].lineRepresentation());
+       /*AdminRole aa=new AdminRole();
+       aa.removeEmployee("5");
+     */
+
     }
 }
