5ebed8893bfc883d8ea8c104a7905c40f0921afc
mohand <mohandamr2019@gmail.com>
2025-10-16 00:38:27 +0200

    Added public to main

--- src/mai.java
+++ src/mai.java
@@ -1,16 +1,23 @@
 import databases.EmployeeUserDatabase;
+import databases.ProductDatabase;
 import users.EmployeeUser;
-
+import products.Product;
 import java.io.IOException;
 
 public class mai {
 
-    static void main(String[] args) throws IOException {
+   public static void main(String[] args) throws IOException {
         EmployeeUser e1 = new EmployeeUser("5","2","3","4","5");
         EmployeeUserDatabase db = new EmployeeUserDatabase("Employees.txt");
 
+        ProductDatabase pdb = new ProductDatabase("Products.txt");
+        Product p1 = new Product("1","2","3","4",10, 1000.0F);
         db.readFromFile();
         db.insertRecord(e1);
         db.saveToFile();
+
+        pdb.readFromFile();
+        pdb.insertRecord(p1);
+        pdb.saveToFile();
     }
 }
