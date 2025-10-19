0eb794abfdc2eda9c393478220cb2aab95328141
moatasem-1 <mutasemmohamed24@gmail.com>
2025-10-19 22:11:21 +0300

    Update NameValidator.java

--- src/validations/NameValidator.java
+++ src/validations/NameValidator.java
@@ -1,4 +1,17 @@
 package validations;
+import java.io.*;
 
 public class NameValidator {
+  
+public static boolean check_Name(String Name){
+  if(Name==null)
+      return false;
+     
+  char[]name=Name.toCharArray();
+     
+  for(int i=0;i<Name.length();i++){
+     if(!Character.isLetter(name[i]))   
+        return false;}
+  
+     return true; }
 }
