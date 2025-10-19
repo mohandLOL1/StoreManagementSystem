4c494e78c77b98dcf245768d32fdbedd37045fa5
Moatez-000 <moateez.ahmed@gmail.com>
2025-10-19 22:23:39 +0300

    PhoneValidator.java

--- src/validations/PhoneValidator.java
+++ src/validations/PhoneValidator.java
@@ -1,8 +1,15 @@
 package validations;
+public class PhoneValidator {
 
-public class PhoneValidator{
     public static boolean validate(String number) {
 
+        if (number.trim().length() != 11) return false;
+        else {
+            char[] char_number = number.toCharArray();
+            for (char c : char_number) {
+                if (!Character.isDigit(c)) return false;
+            }
+        }
         return true;
     }
 }
